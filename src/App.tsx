b6280fbe266c7042e9bc6b8d565b741bf6c75720
Gustavo Murayama <gmurayama@outlook.com>
2019-05-15 17:23:30 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from './logo.svg';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <header>
+        <div>languages</div>
+        <h1>/GM</h1>
+      </header>
+      <section>
+        bar
+      </section>
+      <article>
+        about
+      </article>
+    </>
+  );
+}
+
+export default App;
