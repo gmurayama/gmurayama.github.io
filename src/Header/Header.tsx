2c8e3e661bb7db44dcc0edf3758f5e0f1b831051
Gustavo Murayama <gmurayama@outlook.com>
2019-05-16 10:00:50 -0300

    Added reusable component Container

--- src/Header/Header.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styled from 'styled-components';
-
-const Header = styled.header`
-    text-align: right;
-
-`;
---
+++ src/components/Container.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    display: grid;
+    grid-template-columns: repeat(12, 1fr);
+    grid-gap: 15px;
+`;
