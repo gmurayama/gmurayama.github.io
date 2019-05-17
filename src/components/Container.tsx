661ed971c0f09e91bd783fb618ec38677a9ba13c
Gustavo Murayama <gmurayama@outlook.com>
2019-05-17 10:56:21 -0300

    Custom prop to set the number of columns in Container

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
 import styled from 'styled-components';
 
-export const Container = styled.div`
+interface IProps extends React.HTMLAttributes<any> {
+    gridTemplateColumns?: string;
+}
+
+export const Container = styled.div<IProps>`
     display: grid;
-    grid-template-columns: repeat(12, 1fr);
+    grid-template-columns: ${props => props.gridTemplateColumns == null ? "repeat(12, 1fr)" : props.gridTemplateColumns};
     grid-gap: 15px;
+    max-width: 80em;
+    margin: 0 auto;
+    width: 100%;
 `;
