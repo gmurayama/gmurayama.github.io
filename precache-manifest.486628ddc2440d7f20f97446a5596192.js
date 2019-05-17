ec889e6d1abee8f45365d5750a267fdd05c8cc00
Gustavo Murayama <gmurayama@outlook.com>
2019-05-17 11:17:32 -0300

    Updates

---
+++ precache-manifest.486628ddc2440d7f20f97446a5596192.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "617d436dfe59b919aaf092aad03f9e7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f08aa87f320bee03ce86",
+    "url": "/static/css/main.2e58b7d4.chunk.css"
+  },
+  {
+    "revision": "97c3959ff738fdc91098",
+    "url": "/static/js/2.e12a97e5.chunk.js"
+  },
+  {
+    "revision": "f08aa87f320bee03ce86",
+    "url": "/static/js/main.73a1b4e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5705f21133536d7700e71506477b8237",
+    "url": "/static/media/icomoon.5705f211.woff"
+  },
+  {
+    "revision": "57cc80f554697bf05a861e26fb82ab23",
+    "url": "/static/media/icomoon.57cc80f5.svg"
+  },
+  {
+    "revision": "71258f660b269dc86c8916561a959619",
+    "url": "/static/media/icomoon.71258f66.eot"
+  },
+  {
+    "revision": "7744584a912e764d7ee525e5a380a7d3",
+    "url": "/static/media/icomoon.7744584a.ttf"
+  }
+]);
