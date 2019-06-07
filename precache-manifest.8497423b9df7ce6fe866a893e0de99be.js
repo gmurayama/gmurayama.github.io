f5dfe32ff170125aa8d4f32be6d7892c023cad1b
Gustavo Murayama <gmurayama@outlook.com>
2019-06-07 19:49:54 -0300

    Updates

---
+++ precache-manifest.8497423b9df7ce6fe866a893e0de99be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e97e4d43b5ec13a7de9353768ea42781",
+    "url": "https://gmurayama.dev/index.html"
+  },
+  {
+    "revision": "4651b4a24f0aecb6068e",
+    "url": "https://gmurayama.dev/static/css/main.4095ddfc.chunk.css"
+  },
+  {
+    "revision": "d4eac9e0c2d65d6c6f0a",
+    "url": "https://gmurayama.dev/static/js/2.23a74dcd.chunk.js"
+  },
+  {
+    "revision": "4651b4a24f0aecb6068e",
+    "url": "https://gmurayama.dev/static/js/main.e997e105.chunk.js"
+  },
+  {
+    "revision": "6b0bb3c51b37404b3b24",
+    "url": "https://gmurayama.dev/static/js/runtime~main.f845d342.js"
+  },
+  {
+    "revision": "035751a7817a29ad6afbdb2457363f75",
+    "url": "https://gmurayama.dev/static/media/icomoon.035751a7.svg"
+  },
+  {
+    "revision": "5705f21133536d7700e71506477b8237",
+    "url": "https://gmurayama.dev/static/media/icomoon.5705f211.woff"
+  },
+  {
+    "revision": "71258f660b269dc86c8916561a959619",
+    "url": "https://gmurayama.dev/static/media/icomoon.71258f66.eot"
+  },
+  {
+    "revision": "7744584a912e764d7ee525e5a380a7d3",
+    "url": "https://gmurayama.dev/static/media/icomoon.7744584a.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee168f8bf4b0de12d66e70f80b831c19.js"
+  "https://gmurayama.dev/precache-manifest.8497423b9df7ce6fe866a893e0de99be.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://gmurayama.dev/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
